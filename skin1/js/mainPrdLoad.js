343d2e2ee17ba1ec61c1724a280eb25b9a1fa15a
endurenova <endurenova@gmail.com>
2024-01-25 19:00:39 +0900

    [update] 2024-01-25-03

--- skin1/js/mainPrdLoad.js
+++ skin1/js/mainPrdLoad.js
@@ -2,15 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.main_product_list').insertAdjacentHTML(
     'afterend',
     `
-      <section>
-        <div class="main_product__box" style="display:none">
-          <ul class="main_product__list">
+      <section class="main_prd__section">
+        <div class="main_title ez-align-center">
+          <div class="main_title_txt01">
+            전세계에서 가장 주목받고 있는 작품
+          </div>
+        </div>
+        <div class="main_prd__box">
+          <ul class="main_prd__list">
           </ul>
         </div>
       </section>
     `,
   );
-  const mainPrdList = document.querySelector('.main_product__list');
+  const mainPrdList = document.querySelector('.main_prd__list');
   fetch('/js/mainPrdList.json')
     .then(res => res.json())
     .then(data =>
@@ -20,30 +25,30 @@ document.addEventListener('DOMContentLoaded', () => {
           `
           <li>
             <div class="prdList__item">
-                <div class="thumbnail">
+              <div class="thumbnail">
+                <a href="${e['prdUrl']}">
+                  <img src="${e['prdImg']}"  alt="${e['prdAlt']}" />
+                </a>
+              </div>
+              <div class="description">
+                <div class="name">
                   <a href="${e['prdUrl']}">
-                      <img src="${e['prdImg']}"  alt="${e['prdAlt']}" />
+                    <span>${e['prdName']}</span>
                   </a>
                 </div>
-                <div class="description">
-                    <div class="name">
-                        <a href="${e['prdUrl']}">
-                            <span>${e['prdName']}</span>
-                        </a>
-                    </div>
-                    <ul class="spec">
-                      <li>
-                        <span class="artist-name">
-                            <span class="korean-pronun">${e['prdKor']}</span>
-                            <span>${e['prdArtist']}</span>
-                        </span>
-                      </li>
-                    </ul>
-                    <div class="icon">
-                      <strong class="product-item__price">${e['prdPrice']}</strong>
-                    </div>
-                  </div>
+                <ul class="spec">
+                  <li>
+                    <span class="artist-name">
+                      <span class="korean-pronun">${e['prdKor']}</span>
+                      <span>${e['prdArtist']}</span>
+                    </span>
+                  </li>
+                </ul>
+                <div class="icon">
+                  <strong class="product-item__price">${e['prdPrice']}</strong>
+                </div>
               </div>
+            </div>
           </li>
           `,
         );
