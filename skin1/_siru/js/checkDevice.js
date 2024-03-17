2405a358b1cb806b8a3f46cb93e93316d8f1105c
endurenova <endurenova@gmail.com>
2024-03-17 12:53:25 +0900

    [update] 2024-03-17-01

--- skin1/_siru/js/checkDevice.js
+++ skin1/_siru/js/checkDevice.js
@@ -10,5 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
         e.parentElement.classList.toggle('selected');
       });
     });
+  } else {
+    document.body.insertAdjacentHTML(
+      'beforeend',
+      `
+    <img class="ufo-pointer" src="/SkinImg/img/gistar.png" alt="지스타" />`,
+    );
+    document.addEventListener('mousemove', e => {
+      let mouseX = e.pageX + 10;
+      let mouseY = e.pageY + 10;
+
+      let cursor = document.querySelector('.ufo-pointer');
+      cursor.style.left = mouseX + 'px';
+      cursor.style.top = mouseY + 'px';
+    });
   }
 });
