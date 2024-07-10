2b5bc92b2caa1c79d52c8dd1bcffd33e6679465b
endurenova <35566770+endurenova@users.noreply.github.com>
2024-07-10 20:52:26 +0900

    [update] 2024-07-10-01

--- skin1/IR/js/IR.js
+++ skin1/IR/js/IR.js
@@ -1,17 +1,13 @@
-window.addEventListener('beforeunload', function () {
-  window.scrollTo(0, 0);
-});
 document.addEventListener('DOMContentLoaded', () => {
   let windowWidth = window.innerWidth;
   let windowHeight = window.innerHeight;
-  const logoTop = document.querySelector('.collector-t');
-  const logoBottom = document.querySelector('.collector-b');
 
   let logoTopSize = 0;
   let logoBottomSize = 0;
   let viewBoxH = 0;
-  const wrapAll = document.querySelectorAll('.wrap');
 
+  const intro = document.querySelector('.parallax-element.title');
+  const subTitle = document.querySelector('.sub-title');
   checkDevice = () => {
     if (windowWidth >= windowHeight) {
       logoTopSize = windowWidth / 25;
@@ -21,64 +17,43 @@ document.addEventListener('DOMContentLoaded', () => {
       } else {
         viewBoxH = windowHeight * 0.3;
       }
-      wrapAll.forEach(section => {
-        if (section.classList.contains('column')) {
-          section.classList.remove('column');
-        }
-      });
+
+      if (document.body.classList.contains('column')) {
+        document.body.classList.remove('column');
+      }
     } else if (windowWidth < windowHeight) {
       logoTopSize = windowWidth / 3;
       logoBottomSize = windowWidth / 4.74;
-      viewBoxH = windowHeight;
-      if (windowHeight >= windowWidth * 2) {
+      if (windowHeight > windowWidth * 2) {
         viewBoxH = windowHeight;
       } else {
         viewBoxH = windowHeight * 1.2;
       }
-      wrapAll.forEach(section => {
-        if (!section.classList.contains('column')) {
-          section.classList.add('column');
-        }
-      });
+      if (!document.body.classList.contains('column')) {
+        document.body.classList.add('column');
+      }
     }
-  };
-
-  checkDevice();
-  window.addEventListener('loaded', function () {
-    window.scrollTo(0, 0);
-  });
-  const intro = document.querySelector('.parallax-element.title');
-  const subTitle = document.querySelector('.sub-title');
-  async function loadingLayer() {
-    await setTimeout(() => {
+    setTimeout(() => {
       intro.innerHTML = `
           <svg class="logo" viewBox="0 0 1320 ${viewBoxH}">
-            <text style='font-size: ${logoTopSize}px;' class='collector-t'x="50%" y="27%" dy="${
-        logoTopSize * 0.96
-      }px" text-anchor="middle">Collector</text>
-            <text style='font-size: ${logoBottomSize}px;' class='collector-b'x="50%" y="52%" dy="${
-        logoBottomSize * 0.96
-      }px" text-anchor="middle">2 0 3 0</text>
+            <text style='font-size: ${logoTopSize}px;' class='collector-t'x="50%" y="27%" dy="${logoTopSize * 0.96}px" text-anchor="middle">Collector</text>
+            <text style='font-size: ${logoBottomSize}px;' class='collector-b'x="50%" y="52%" dy="${logoBottomSize * 0.96}px" text-anchor="middle">2 0 3 0</text>
           </svg>
         `;
     }, 1000);
-  }
+    setTimeout(() => {
+      subTitle.classList.add('active');
+      document.body.style.overflowY = 'auto';
+    }, 3000);
+  };
 
-  loadingLayer();
-  setTimeout(() => {
-    subTitle.classList.add('active');
-  }, 3000);
-  setTimeout(() => {
-    document.body.style.overflowY = 'auto';
-  }, 3000);
+  checkDevice();
 
   window.addEventListener('resize', function () {
     windowWidth = window.innerWidth;
     windowHeight = window.innerHeight;
 
     checkDevice();
-    logoTop.style.font = `normal normal normal ${logoTopSize}px MinervaMorden-Black`;
-    logoBottom.style.font = `normal normal normal ${logoBottomSize}px MinervaMorden-Black`;
   });
   window.addEventListener('scroll', function () {
     // 스크롤 이벤트 리스너 등록
@@ -88,7 +63,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const title = sections.querySelector('.title'); // 제목 요소
     const gisterUFO = sections.querySelector('.gister-UFO');
     const gistarUFO = sections.querySelector('.gistar-UFO');
-    const gisterCyborg = sections.querySelector('.gister-cyborg');
 
     const scrolled = window.scrollY; // 윈도우의 스크롤값
 
--- skin1/IR/js/fullPage.js
+++ skin1/IR/js/fullPage.js
@@ -1,3 +1,40 @@
 new fullpage('#fullpage', {
   // options here
+  licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+  loopTop: false, // 첫번째 구역에서 스크롤을 위로 올릴 때 마지막 구역으로 이동할지 여부를 정의합니다.
+  loopBottom: false, //마지막 구역에서 스크롤을 아래로 내릴 때 첫번째 구역으로 이동할지 여부를 정의합니다.
+  navigation: true,
+  navigationPosition: 'right',
+});
+const headerBar = document.querySelector('#header');
+let prevScrollpos = 0;
+window.addEventListener('scroll', event => {
+  event.preventDefault();
+  const currentScrollPos = window.scrollY;
+  if (prevScrollpos < currentScrollPos) {
+    headerBar.classList.add('hidden');
+  } else {
+    headerBar.classList.remove('hidden');
+  }
+
+  prevScrollpos = currentScrollPos;
+  console.log(window.scrollY);
+});
+checkDevice = () => {
+  let windowWidth = window.innerWidth;
+  let windowHeight = window.innerHeight;
+  if (windowWidth >= windowHeight) {
+    if (document.body.classList.contains('column')) {
+      document.body.classList.remove('column');
+    }
+  } else if (windowWidth < windowHeight) {
+    if (!document.body.classList.contains('column')) {
+      document.body.classList.add('column');
+    }
+  }
+};
+checkDevice();
+
+window.addEventListener('resize', function () {
+  checkDevice();
 });
